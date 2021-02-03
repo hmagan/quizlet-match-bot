41b1dc897375352783852c56394ac5bf7fb3506d
Hank Magan <bryant.magan@gmail.com>
2021-02-03 09:37:39 -0500

    now works even if term has extra formatting

--- answers.js
+++ answers.js
@@ -1,14 +1,40 @@
 //find all of the terms and their definitions
 let terms = document.getElementsByClassName("TermText");
-let arrayOfTerms = [];
+let list = "[";
 
 for(let i = 0; i < terms.length; i++){
-    //push them to an array
-    arrayOfTerms.push(terms[i].innerHTML);
-    console.log(terms[i].innerHTML);
+    let term = terms[i].innerHTML;
+    //if a term has double quotes, escape them
+    for(let j = 0; j < term.length; j++){
+        if(term.substring(j, j+1) === "\"") {
+            term = term.substring(0, j) + "\\" + term.substring(j);
+            j++;
+        }
+    }
+    console.log(term);
+    //add term to list in quotes
+    list += "\"" + term + "\", ";
+}
+
+//eliminate the extra comma and space and add a closing bracket
+list = list.substring(0, list.length-2);
+list += "]";
+
+//remove formatting that messes with the terms
+for(let i = 0; i < list.length; i++){
+    if(list.substring(i, i+1) === "<"){
+        //removes <div>, <span>, <p> and closing tags i.e. </p>
+        if(i != list.length-1 && (list.substring(i+1, i+2) === "/" || list.substring(i+1, i+2) === "d") || list.substring(i+1, i+2) === "s" || list.substring(i+1, i+2) === "p"){
+            while(list.substring(i, i+1) !== ">"){
+                list = list.substring(0, i) + list.substring(i + 1);
+            }
+            list = list.substring(0, i) + list.substring(i + 1);
+            i--;
+        }
+    }
 }
 
 //print the array for the user to copy
 console.log("\nCOPY THIS VALUE (without the number in parentheses):");
-console.log(arrayOfTerms);
-console.log("PASTE ABOVE bot.js");
+console.log(list);
+console.log("PASTE ABOVE into bot.js");
