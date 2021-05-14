aa18e6ec72a531b13b1c30a6941ae3bef478bc7c
Hank Magan <bryant.magan@gmail.com>
2021-05-13 22:10:52 -0400

    removed excess commenting

--- answers.js
+++ answers.js
@@ -1,10 +1,8 @@
-//find all of the terms and their definitions
 let terms = document.getElementsByClassName("TermText");
 let list = "[";
 
 for(let i = 0; i < terms.length; i++){
     let term = terms[i].innerHTML;
-    //if a term has double quotes, escape them
     for(let j = 0; j < term.length; j++){
         if(term.substring(j, j+1) === "\"") {
             term = term.substring(0, j) + "\\" + term.substring(j);
@@ -12,15 +10,12 @@ for(let i = 0; i < terms.length; i++){
         }
     }
     console.log(term);
-    //add term to list in quotes
     list += "\"" + term + "\", ";
 }
 
-//eliminate the extra comma and space and add a closing bracket
 list = list.substring(0, list.length-2);
 list += "]";
 
-//remove formatting that messes with the terms
 for(let i = 0; i < list.length; i++){
     if(list.substring(i, i+1) === "<"){
         //removes <div>, <span>, <p> and closing tags i.e. </p>
@@ -34,7 +29,6 @@ for(let i = 0; i < list.length; i++){
     }
 }
 
-//print the array for the user to copy
 console.log("\nCOPY THIS ARRAY:");
 console.log(list);
 console.log("PASTE INTO bot.js IN THE flashcards VARIABLE");
